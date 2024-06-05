83f33372907a9c4248f89cdd912144a838208339
Hala7123 <167092056+Hala7123@users.noreply.github.com>
2024-06-05 12:40:17 +1000

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,128 +4,92 @@ pipeline {
     stages {
         stage('Building Process') {
             steps {
-                script {
-                    def logFile = "build_log.txt"
-                    bat "echo Starting the construction process. Let the magic begin! > ${logFile}" // For Windows
-                    // sh "echo 'Starting the construction process. Let the magic begin!' | tee ${logFile}" // For Unix/Linux
-                }
+                echo 'Starting the construction process.'
+                
             }
         }
 
         stage('Unit and Integration Checks') {
             steps {
-                script {
-                    def logFile = "unit_integration_log.txt"
-                    bat "echo Using JUnit to carry out unit checks. Fingers crossed! > ${logFile}" // For Windows
-                    bat "echo Applying JUnit to integration tests. Hold tight! >> ${logFile}" // For Windows
-                    // sh "echo 'Using JUnit to carry out unit checks. Fingers crossed!' | tee ${logFile}" // For Unix/Linux
-                    // sh "echo 'Applying JUnit to integration tests. Hold tight!' | tee -a ${logFile}" // For Unix/Linux
-                }
+                echo 'Using JUnit to carry out unit checks.'
+                echo 'Applying JUnit to integration tests.'
+               
             }
             post {
                 success {
-                    emailext to: 'hala070304@gmail.com',
-                             subject: 'Checks Passed',
-                             body: 'Every check was successfully completed. Great job team!',
-                             attachLog: true
+                    mail to: 'hala070304@gmail.com',
+                         subject: 'Checks Passed',
+                         body: 'Every check was successfully completed.'
                 }
                 failure {
-                    emailext to: 'hala070304@gmail.com',
-                             subject: 'Check Failures',
-                             body: 'A few of the checks were unsuccessful. Please review the outcomes and don’t lose hope!',
-                             attachLog: true,
-                             attachmentsPattern: 'unit_integration_log.txt'
+                    mail to: 'hala070304@gmail.com',
+                         subject: 'Check Failures',
+                         body: 'A few of the checks were unsuccessful. Please review the outcomes.'
                 }
             }
         }
 
         stage('Analyzing Code') {
             steps {
-                script {
-                    def logFile = "code_analysis_log.txt"
-                    bat "echo Analyzing code. Using SonarScanner to perform SonarQube analysis. Time to find those bugs! > ${logFile}" // For Windows
-                    // sh "echo 'Analyzing code. Using SonarScanner to perform SonarQube analysis. Time to find those bugs!' | tee ${logFile}" // For Unix/Linux
-                }
+                echo 'analyzing codes. Using SonarScanner to perform SonarQube analysis.'
+                
             }
         }
 
         stage('Security Check') {
             steps {
-                script {
-                    def logFile = "security_check_log.txt"
-                    bat "echo OWASP ZAP (Zed Attack Proxy) is used to conduct security audits. Let’s keep our app safe! > ${logFile}" // For Windows
-                    // sh "echo 'OWASP ZAP (Zed Attack Proxy) is used to conduct security audits. Let’s keep our app safe!' | tee ${logFile}" // For Unix/Linux
-                }
+                echo 'OWASP ZAP (Zed Attack Proxy) is used to conduct security audits..'
+                
             }
             post {
                 success {
-                    emailext to: 'hala070304@gmail.com',
-                             subject: 'Security Check Passed',
-                             body: 'No security flaws were found. Excellent work on keeping things secure!',
-                             attachLog: true
+                    mail to: 'hala070304@gmail.com',
+                         subject: 'Security Check Passed',
+                         body: 'No security flaws were found..'
                 }
                 failure {
-                    emailext to: 'hala070304@gmail.com',
-                             subject: 'Security Check Failed',
-                             body: 'Security flaws discovered. Needs immediate attention. Stay vigilant!',
-                             attachLog: true,
-                             attachmentsPattern: 'security_check_log.txt'
+                    mail to: 'hala070304@gmail.com',
+                         subject: 'Security Check Failed',
+                         body: 'discovered security flaws. Needs immediate attention.'
                 }
             }
         }
 
         stage('Deploying to Staging Environment') {
             steps {
-                script {
-                    def logFile = "staging_deploy_log.txt"
-                    bat "echo Deploying the application to a staging environment (e.g., AWS EC2 instance). Almost there! > ${logFile}" // For Windows
-                    // sh "echo 'Deploying the application to a staging environment (e.g., AWS EC2 instance). Almost there!' | tee ${logFile}" // For Unix/Linux
-                }
+                echo 'Deploying the application to a staging environment (e.g., AWS EC2 instance)...'
+                
             }
         }
 
         stage('Staging Integration Tests') {
             steps {
-                script {
-                    def logFile = "staging_integration_log.txt"
-                    bat "echo Performing integration tests in the staging environment. Hang in there! > ${logFile}" // For Windows
-                    // sh "echo 'Performing integration tests in the staging environment. Hang in there!' | tee ${logFile}" // For Unix/Linux
-                }
+                echo 'Performing integration tests in the staging environment...'
+                
             }
             post {
                 success {
-                    emailext to: 'hala070304@gmail.com',
-                             subject: 'Staging Integration Tests Passed',
-                             body: 'Successful completion of the staging integration tests. Ready for the next step!',
-                             attachLog: true
+                    mail to: 'hala070304@gmail.com',
+                         subject: 'Staging Integration Tests Passed',
+                         body: 'Successful completion of the staging integration tests.'
                 }
                 failure {
-                    emailext to: 'hala070304@gmail.com',
-                             subject: 'Staging Integration Tests Failed',
-                             body: 'Staging integration tests were unsuccessful. Additional research is necessary. Let’s fix it!',
-                             attachLog: true,
-                             attachmentsPattern: 'staging_integration_log.txt'
+                    mail to: 'hala070304@gmail.com',
+                         subject: 'Staging Integration Tests Failed',
+                         body: 'Staging integration tests were unsuccessful. Additional research is necessary.'
                 }
             }
         }
 
         stage('Deploying to Production') {
             steps {
-                script {
-                    def logFile = "production_deploy_log.txt"
-                    bat "echo Deploying the application to a production environment (e.g., AWS EC2 instance). Almost live! > ${logFile}" // For Windows
-                    // sh "echo 'Deploying the application to a production environment (e.g., AWS EC2 instance). Almost live!' | tee ${logFile}" // For Unix/Linux
-                }
+                echo 'Deploying the application to a production environment (e.g., AWS EC2 instance).'
             }
         }
 
         stage('Committing Changes') {
             steps {
-                script {
-                    def logFile = "commit_changes_log.txt"
-                    bat "echo Successfully committed changes. Great work team! > ${logFile}" // For Windows
-                    // sh "echo 'Successfully committed changes. Great work team!' | tee ${logFile}" // For Unix/Linux
-                }
+                echo 'successfully committed changes.'
             }
         }
     }
